2c5faf8264367052e313c02bf904cbc796d03d9b
kumol <kumol01763@gmail.com>
2024-08-23 00:24:31 +0600

    first commit

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require('dotenv').config();
+require("./config/db");
+const route = require("./server/api/index");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/api",route);
+app.listen(process.env.PORT,(error)=>{
+    if(error) console.log(error);
+    else console.log("Server is running on port 8080")
+})
